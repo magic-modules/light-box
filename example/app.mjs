aab80250ffe54732ddda3b33999ff65d470931fd
jascha <jascha@jaeh.at>
2020-07-16 23:17:06 +0200

    update logotext

--- example/app.mjs
+++ example/app.mjs
@@ -2,6 +2,7 @@ export const state = {
   title: '@magic-modules/light-box',
   description: 'floating images that show a bigger view when clicked.',
   logotext: 'light-box',
+
   menu: [
     { to: '/#installation', text: 'installation' },
     { to: '/#usage', text: 'usage' },
